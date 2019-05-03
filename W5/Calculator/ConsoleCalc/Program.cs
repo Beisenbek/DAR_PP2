041c732030ea99b3a73f7f33cad0e17042495948
Beisenbek Baisakov <beysenbek@gmail.com>
2019-05-03 11:04:55 +0600

    'upd'

---
+++ W5/Calculator/ConsoleCalc/Program.cs
@@ -0,0 +1,31 @@
+﻿using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            MyDelegate myDelegate = new MyDelegate(DisplayText2);
+            Brain brain = new Brain(myDelegate);
+
+            while (true)
+            {
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                brain.Process(consoleKeyInfo.KeyChar.ToString());
+            }
+        }
+
+        static void DisplayText2(string msg)
+        {
+            Console.Clear();
+            Console.Write(msg);
+        }
+    }
+}
